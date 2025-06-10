41602ba40200600ff6c4c4389c061267b1fd17d5
alpyhuntoyungo17 <shiroyasha391@gmail.com>
2025-06-11 05:49:57 +0800

    update

--- travel-backend-master/server.js
+++ travel-backend-master/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api", destinasiRouter);
 // app.use("/api", usersRouter);
