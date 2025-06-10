c4bed5bec4694285431e981dc378206de0cd5a7d
alpyhuntoyungo17 <shiroyasha391@gmail.com>
2025-06-11 06:14:51 +0800

    update

--- travel-backend-master/router/destinasiRouter.js
+++ travel-backend-master/router/destinasiRouter.js
@@ -4,6 +4,19 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
+const claudinary = require('cloudinary')
+
+claudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+
+
+
+
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -31,13 +44,28 @@ router.get("/data", async (req, res) => {
 
 router.post("/post", upload.single("gambar"), async (req, res) => {
   const { nama, tempat, lokasi, deskripsi } = req.body;
-  const gambar = req.file ? req.file.filename : null;
+  const gambar = req.file?.path;
 
   try {
+
+      let cloudinaryData = null;
+
+    if (gambar) {
+      cloudinaryData = await cloudinary.uploader.upload(gambar, {
+        resource_type: "auto",
+      });
+      // console.log(cloudinaryData);
+
+      fs.unlinkSync(gambar);
+    }
+    let fileUrl = cloudinaryData?.secure_url || null;
+
+
+
     const result = await db.query(
       `INSERT INTO informasi_tampat (nama, tempat, lokasi, gambar, deskripsi)
        VALUES ($1, $2, $3, $4, $5)`,
-      [nama, tempat, lokasi, gambar, deskripsi]
+      [nama, tempat, lokasi, fileUrl, deskripsi]
     );
 
     res.status(201).json({
