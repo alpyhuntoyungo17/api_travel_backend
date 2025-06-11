ac8eb6f63ab56497b94728d0837d852c175c1e77
alpyhuntoyungo17 <shiroyasha391@gmail.com>
2025-06-11 19:03:42 +0800

    update

--- travel-backend-master/router/userRouter.js
+++ travel-backend-master/router/userRouter.js
@@ -14,6 +14,25 @@ router.get("/get", async (req, res) => {
   }
 });
 
+
+router.get("/get/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await db.query("SELECT * FROM users WHERE id = $1", [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "user tidak ditemukan" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error saat mengambil user berdasarkan ID:", error);
+    res.status(500).json({ message: "Gagal mengambil user" });
+  }
+});
+
+
 // Tambahkan user baru
 router.post("/add", async (req, res) => {
   const { username, email, password } = req.body;
